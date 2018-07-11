a8cff688375371da8408f3757cf44c48738b0ecb
John Beech <johnbeech.github@mkv25.net>
2018-07-11 21:40:15 +0100

    Implemented a new class of test using Nightmare

--- test/toggle-spec.js
+++ test/toggle-spec.js
@@ -1,9 +1,27 @@
 const { expect } = require('chai')
-const { fetch } = require('promise-path')
+const Nightmare = require('nightmare')
 const environment = require('./helpers/environment')
 
+function dream(vision) {
+  console.error('[Nightmare] Disturbed by', vision)
+}
+
 describe(`Quick Score Toggle Items [${environment.name}]`, () => {
-    it(`should display an item to toggle`)
-    it(`should allow a user to select an unselected item`)
-    it(`should allow a user to deselect a selected item`)
+  let nightmare
+  beforeEach(() => {
+    nightmare = Nightmare()
+  })
+
+  it(`should display an item to toggle`, async () => {
+    const title = await nightmare
+      .goto(`${environment.serviceUrl}`)
+      .evaluate(() => document.querySelector('h1').textContent)
+      .end()
+      .catch(dream)
+
+    expect(title).to.equal('Quick Score')
+  })
+
+  it(`should allow a user to select an unselected item`)
+  it(`should allow a user to deselect a selected item`)
 })
