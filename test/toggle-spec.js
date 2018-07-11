e9b3ecf96d38677bd71322391d562fe027b1f4e4
John Beech <johnbeech.github@mkv25.net>
2018-07-11 19:09:07 +0100

    Write pending specs for the Toggleable score item milestone

---
+++ test/toggle-spec.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai')
+const { fetch } = require('promise-path')
+const environment = require('./helpers/environment')
+
+describe(`Quick Score Toggle Items [${environment.name}]`, () => {
+    it(`should display an item to toggle`)
+    it(`should allow a user to select an unselected item`)
+    it(`should allow a user to deselect a selected item`)
+})
