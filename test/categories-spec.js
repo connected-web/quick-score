df9518a0195e9c8b6d3af1b72ee31c9eb28e07e1
John Beech <johnbeech.github@mkv25.net>
2018-07-21 20:43:17 +0100

    Implement score totals on a per category basis

--- test/categories-spec.js
+++ test/categories-spec.js
@@ -27,11 +27,24 @@ describe(`Quick Score Categories [${environment.name}]`, () => {
   it(`should have multiple categories with a separate heading and score`, async () => {
     const raw = await nightmare
       .goto(`${environment.serviceUrl}`)
-      .evaluate(() => Array.from(document.querySelectorAll('.category.title')).map(el => el.textContent).join(', '))
+      .evaluate(() => Array.from(document.querySelectorAll('.category.title')).map(el => el.textContent.trim()).join(', '))
       .end()
       .catch(dream)
     const actual = (raw + '').trim()
 
     expect(actual).to.equal('Engineering Practice 0, Communication 0')
+  }).timeout(5000)
+
+  it(`should only total up the score for items in the category being toggled`, async () => {
+    const actual = await nightmare
+      .goto(`${environment.serviceUrl}`)
+      .click('button.toggle.option:nth-of-type(2)')
+      .click('button.toggle.option:nth-of-type(3)')
+      .click('button.toggle.option:nth-of-type(5)')
+      .evaluate(() => Array.from(document.querySelectorAll('.score.panel')).map(el => el.textContent.trim()).join(', '))
+      .end()
+      .catch(dream)
+
+    expect(actual).to.equal('Engineering Practice 2, Communication 1, Overall 3')
   }).timeout(5000)
 })
