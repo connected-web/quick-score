6ee7cbd317e81d93563ef97c97474d2fc51a4d1d
John Beech <johnbeech.github@mkv25.net>
2018-07-27 06:40:39 +0100

    Move config for engineer categories into separate file

--- data/engineer-skills.js
+++ data/engineer-skills.js
@@ -1,1 +1,30 @@
-console.log('Engineer skills included')
+const create = () => {
+  return {
+    totalScore: 0,
+    categories: [,
+      {
+        title: 'Communication',
+        options: ['Clearly written', 'Concise points', 'Highlighting key facts', 'Key skills', 'Supporting evidence'],
+        score: 0
+      },
+      {
+        title: 'Delivery',
+        options: ['Complete delivery of a small project', 'Regular delivery over 1 year period', 'Scrum team', 'Daily standups', 'Kanban board', 'Story based work', 'Estimation technique', 'Collaborative prioritisation', 'Retrospective improvement'],
+        score: 0
+      },
+      {
+        title: 'Engineering Practice',
+        options: ['Unit Testing', 'Automation Pipeline', 'Linting', 'Pair Programming', 'Source Control', 'Code Reviews', 'Auditing & Logs'],
+        score: 0
+      }
+    ]
+  }
+}
+
+if (typeof window !== 'undefined') {
+  window.EngineerSkills = { create }
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = { create }
+}
